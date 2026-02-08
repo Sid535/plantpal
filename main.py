2cbe1fa8db88a8522a48c51a05a73a439418f48c
Siddesh <115077155+Sid535@users.noreply.github.com>
2026-02-08 11:49:34 +0000

    testing streamit in test branch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from plantpal!")
+import subprocess
+import sys
 
+def main():
+    print("Launching PlantPal Client...")
+    # This runs 'streamlit run client/app.py'
+    subprocess.run([sys.executable, "-m", "streamlit", "run", "client/app.py"])
 
 if __name__ == "__main__":
     main()
