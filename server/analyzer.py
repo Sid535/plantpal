5439fad65c6507cf0f39c777892462b2e7d72112
sid535 <115077155+Sid535@users.noreply.github.com>
2026-02-21 19:48:09 +0530

    created the setup for the training the model

--- server/analyzer.py
+++ server/analyzer.py
@@ -1,10 +1,6 @@
 # server/analyzer.py
 
 def analyze_plant_image(image_file):
-    """
-    Simulates plant and disease identification.
-    In the future, you can replace this with a real AI model (like TensorFlow or PyTorch).
-    """
     if image_file is not None:
         # Mock results for demonstration
         result = {
