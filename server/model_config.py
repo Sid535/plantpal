8cad7d22905226ee1be769815d12b19a069bd3fa
sid535 <115077155+Sid535@users.noreply.github.com>
2026-02-23 01:50:12 +0530

    Created the first working model

--- server/model_config.py
+++ server/model_config.py
@@ -1,4 +1,4 @@
-BATCH_SIZE = 8 
+BATCH_SIZE = 8
 IMAGE_SIZE = (224, 224)
 
 
@@ -14,7 +14,7 @@ tomato_classes = [
     'Tomato___Late_blight',
     'Tomato___Leaf_Mold',
     'Tomato___Septoria_leaf_spot',
-    'Tomato___Spider_mites_Two-spotted_spider_mite',
+    'Tomato___Spider_mites Two-spotted_spider_mite',
     'Tomato___Target_Spot',
     'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
     'Tomato___Tomato_mosaic_virus',
