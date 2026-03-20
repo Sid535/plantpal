99e7baf5b040c731ee157f4a91bd44144a9beb8c
sid535 <115077155+Sid535@users.noreply.github.com>
2026-03-20 13:04:20 +0530

    updated the class list

--- server/model_config.py
+++ server/model_config.py
@@ -20,10 +20,76 @@ EARLY_STOP_PATIENCE = 3
 BRIGHTNESS_FACTOR = 0.2
 CONTRAST_FACTOR = 0.2
 
+apple_class = [
+    'Apple___Apple_scab',
+    'Apple___Black_rot',
+    'Apple___Cedar_apple_rust',
+    'Apple___healthy',
+]
+
+blueberry_class = [
+    'Blueberry___healthy',
+]
+
+cherry_class = [
+    'Cherry_(including_sour)___Powdery_mildew',
+    'Cherry_(including_sour)___healthy',
+]
+
+corn_class = [
+    'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+    'Corn_(maize)___Common_rust_',
+    'Corn_(maize)___Northern_Leaf_Blight',
+    'Corn_(maize)___healthy',
+]
+
+grape_class = [
+    'Grape___Black_rot',
+    'Grape___Esca_(Black_Measles)',
+    'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+    'Grape___healthy',
+]
+
+orange_class = [
+    'Orange___Haunglongbing_(Citrus_greening)',
+]
+
+peach_class = [
+    'Peach___Bacterial_spot',
+    'Peach___healthy',
+]
+
+pepper,_bell_class = [
+    'Pepper,_bell___Bacterial_spot',
+    'Pepper,_bell___healthy',
+]
+
+potato_class = [
+    'Potato___Early_blight',
+    'Potato___Late_blight',
+    'Potato___healthy',
+]
+
+raspberry_class = [
+    'Raspberry___healthy',
+]
+
+soybean_class = [
+    'Soybean___healthy',
+]
+
+squash_class = [
+    'Squash___Powdery_mildew',
+]
+
+strawberry_class = [
+    'Strawberry___Leaf_scorch',
+    'Strawberry___healthy',
+]
+
 tomato_class = [
     'Tomato___Bacterial_spot',
     'Tomato___Early_blight',
-    'Tomato___healthy',
     'Tomato___Late_blight',
     'Tomato___Leaf_Mold',
     'Tomato___Septoria_leaf_spot',
@@ -31,30 +97,25 @@ tomato_class = [
     'Tomato___Target_Spot',
     'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
     'Tomato___Tomato_mosaic_virus',
+    'Tomato___healthy',
 ]
-
-apple_class = [
-    'Apple___Apple_scab',
-    'Apple___Black_rot',
-    'Apple___Cedar_apple_rust',
-    'Apple___healthy'
-]
-
-corn_class = [
-    "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot",
-    "Corn_(maize)___Common_rust_",
-    "Corn_(maize)___healthy",
-    "Corn_(maize)___Northern_Leaf_Blight"
-]
-
-potato_class = []
 
 ALL_CLASSES = [
     *apple_class,
+    *blueberry_class,
+    *cherry_class,
     *corn_class,
+    *grape_class,
+    *orange_class,
+    *peach_class,
+    *pepper,_bell_class,
+    *potato_class,
+    *raspberry_class,
+    *soybean_class,
+    *squash_class,
+    *strawberry_class,
     *tomato_class,
 ]
-
 
 PLANT_CONFIG = {
     "apple": {"classes": apple_class},
